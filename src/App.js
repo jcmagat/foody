b8a152a2e9edbc787fdce460049313c35dde5718
Juan Carlo Magat <magatjuancarlo@gmail.com>
2020-06-16 19:41:39 -0700

    Add search to query for recipes
    Also add bootstrap and dotenv

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getQuery = (e) => {
+    e.preventDefault();
+    setQuery(search);
+  };
+
+  const getRecipes = async () => {
+    let results = [];
+
+    if (query !== "") {
+      const response = await fetch(
+        `https://api.spoonacular.com/recipes/search?apiKey=${API_KEY}&query=${query}&number=5`
+      );
+      const data = await response.json();
+      results = data.results;
+    }
+
+    setRecipes(results);
+    console.log(results);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form" onSubmit={getQuery}>
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 }
