ce9740b83d29cf6ddd6d5ab28386c2ed446373a1
Juan Carlo Magat <magatjuancarlo@gmail.com>
2020-06-25 22:10:46 -0700

    Remove dropdown menu from search bar

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,34 +1,10 @@
 import React, { useState } from "react";
-import {
-  InputGroup,
-  Dropdown,
-  DropdownButton,
-  Form,
-  FormControl,
-  Button,
-} from "react-bootstrap";
+import { InputGroup, Form, FormControl, Button } from "react-bootstrap";
 
 const SearchForm = (props) => {
-  const [dropdownTitle, setDropdownTitle] = useState("Recipe Name");
-
-  const updateDropdownTitle = (e) => {
-    setDropdownTitle(e.target.text);
-  };
-
   return (
     <Form onSubmit={props.updateSearch}>
       <InputGroup>
-        <InputGroup.Prepend>
-          <DropdownButton id="dropdown-basic-button" title={dropdownTitle}>
-            <Dropdown.Item href="#" onClick={updateDropdownTitle}>
-              Recipe Name
-            </Dropdown.Item>
-            <Dropdown.Item href="#" onClick={updateDropdownTitle}>
-              Ingredients
-            </Dropdown.Item>
-          </DropdownButton>
-        </InputGroup.Prepend>
-
         <FormControl type="text" name="search" />
 
         <InputGroup.Append>
